f10bc63470477b4acc941a1a5287b691a8354e68
LanLop1 <lanchazolopezs@gmail.com>
2024-07-22 19:16:10 +0200

    modified:   Django_SearchE/django-starter/README.md modified:   Django_SearchE/django-starter/a_core/__pycache__/__init__.cpython-312.pyc modified:   Django_SearchE/django-starter/a_core/__pycache__/settings.cpython-312.pyc modified:   Django_SearchE/django-starter/a_core/__pycache__/urls.cpython-312.pyc modified:   Django_SearchE/django-starter/a_core/__pycache__/wsgi.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/__pycache__/__init__.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/__pycache__/admin.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/__pycache__/apps.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/__pycache__/models.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/__pycache__/views.cpython-312.pyc new file:   Django_SearchE/django-starter/a_home/migrations/0002_article_image.py modified:   Django_SearchE/django-starter/a_home/migrations/__pycache__/0001_initial.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/migrations/__pycache__/__init__.cpython-312.pyc modified:   Django_SearchE/django-starter/a_home/models.py modified:   Django_SearchE/django-starter/a_users/__pycache__/__init__.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/admin.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/apps.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/forms.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/models.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/signals.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/urls.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/__pycache__/views.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/migrations/__pycache__/0001_initial.cpython-312.pyc modified:   Django_SearchE/django-starter/a_users/migrations/__pycache__/__init__.cpython-312.pyc modified:   Django_SearchE/django-starter/db.sqlite3 modified:   Django_SearchE/django-starter/templates/article_detail.html modified:   Django_SearchE/django-starter/templates/search_results.html

--- Django_SearchE/django-starter/a_home/models.py
+++ Django_SearchE/django-starter/a_home/models.py
@@ -3,6 +3,7 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     resumen = models.TextField(max_length=1255)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
